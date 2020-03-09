6453ce49dd7ca5cf2c9bbe296a1206fb7ab5615d
lmkalg <artusopablo+gh_lmkalg@gmail.com>
2020-03-08 23:22:00 -0300

    updating prepare_test_cases script

--- quickxor/tests/prepare_test_cases.sh
+++ quickxor/tests/prepare_test_cases.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 QUICKXOR_DIR=../src/asm
 XOR_IN_C_DIR=../src/c
 KEYS=keys
@@ -6,34 +8,38 @@ FILES=files
 BINARIES=binaries
 OUTPUTS=outputs
 
-echo "[+] Creating dirs.."
+echo "[*] Creating dirs.."
 mkdir -p $KEYS
 mkdir -p $FILES
 mkdir -p $BINARIES
 mkdir -p $OUTPUTS
+echo "[+] Done!"
 
-echo "[+] Creating keys.."
+echo "[*] Creating random keys.."
 for i in `seq 1 16`;
 do
-  echo $i#  dd if=/dev/urandom of=$KEYS/key_$i count=$i bs=1 2> /dev/null
+    dd if=/dev/urandom of=$KEYS/key_$i count=$i bs=1 2> /dev/null
 done
 echo "[+] Done!"
 
-echo "[+] Creating strings"
+echo "[*] Creating random files"
 i=128
 for j in `seq 1 5`;
 do
-#    dd if=/dev/urandom of=$FILES/file_$i count=$i bs=1M 2> /dev/null
+    dd if=/dev/urandom of=$FILES/file_$i count=$i bs=1M 2> /dev/null
     i=$(($i*2))
-    #echo $i
 done
 echo "[+] Done!"
 
-echo "[+] Compiling quickxor"
+echo "[*] Compiling quickxor"
 nasm -f elf64 $QUICKXOR_DIR/quickxor.asm -o $QUICKXOR_DIR/quickxor.o
 gcc $QUICKXOR_DIR/quickxor.o $QUICKXOR_DIR/quickxor.c -o $BINARIES/quickxor
+echo "[+] Done!"
 
-echo "[+] Compiling xor_in_c"
+echo "[*] Compiling xor_in_c"
 gcc $XOR_IN_C_DIR/xor_in_c.c -o $BINARIES/xor_in_c
+echo "[+] Done!"
+
+echo "[+] Everything is ready!"
 
 
